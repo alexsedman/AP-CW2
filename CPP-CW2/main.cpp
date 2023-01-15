d8e7a63d82c01dd33b9951f0e934965195a72692
alexsedman <alexjsedman@gmail.com>
2023-01-15 18:11:14 +0000

    Test 2. Committing to new branch, v2, as commits to main are out of date with remote? idk how but whatever i'll get it eventually

--- CPP-CW2/main.cpp
+++ CPP-CW2/main.cpp
@@ -7,59 +7,77 @@
 struct WAV_HEADER {
     // RIFF Chunk Descriptor
     uint8_t RIFF[4];
-    uint32_t chunk_size;
+    uint32_t file_size;
     uint8_t WAVE[4];
 
     // fmt subchunk
     uint8_t fmt[4];
-    uint32_t subchunk1Size;
-    uint16_t audioFormat;
-    uint16_t numOfChan;
-    uint32_t samplesPerSec;
-    uint32_t bytesPerSec;
-    uint16_t blockAlign;
-    uint16_t bitsPerSample;
+    uint32_t fmt_len;
+    uint16_t fmt_type;
+    uint16_t no_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t sample_len;
     
     // Data subchunk
-    uint8_t subchunk2ID[4];
-    uint32_t subchunk2Size;
+    uint8_t DATA[4];
+    uint32_t data_size;
 } wav_hdr;
 
+// Function to print WAV header.
+void print_header() {
+    using namespace std;
+    //Print the header.
+    cout << "Chunk ID (File Spec): " << wav_hdr.RIFF[0] << wav_hdr.RIFF[1] << wav_hdr.RIFF[2] << wav_hdr.RIFF[3] << endl;
+    cout << "Chunk Size (File Size): " << wav_hdr.file_size << endl;
+    cout << "File Type (FourCC Tag): " << wav_hdr.WAVE[0] << wav_hdr.WAVE[1] << wav_hdr.WAVE[2] << wav_hdr.WAVE[3] << endl;
+    
+    cout << "Subchunk 1 ID (Format): " << wav_hdr.fmt[0] << wav_hdr.fmt[1] << wav_hdr.fmt[2] << wav_hdr.fmt[3] << endl;
+    cout << "Subchunk Size: " << wav_hdr.fmt_len << endl;
+    cout << "Audio Format: " << wav_hdr.fmt_type << endl;
+    cout << "Number of Channels: " << wav_hdr.no_channels << endl;
+    cout << "Sample Rate: " << wav_hdr.sample_rate << endl;
+    cout << "Data Rate (Bps): " << wav_hdr.byte_rate << endl;
+    cout << "Block Align: " << wav_hdr.block_align << endl;
+    cout << "Bits Per Sample: " << wav_hdr.sample_len << endl;
+    
+    cout << "Subchunk 2 ID (Data): " << wav_hdr.DATA[0] << wav_hdr.DATA[1] << wav_hdr.DATA[2] << wav_hdr.DATA[3] << endl;
+    cout << "Sub-Chank2 Size: " << wav_hdr.data_size << endl;
+}
+
 int main() {
-    // Variables declared:
-    int headerSize = sizeof(wav_hdr); // A variable to measure the size of the WAV header
-    const char* filePath;
+    // Variables declared: pointer to the filepath name and an input string.
+    int hdr_size = sizeof(wav_hdr); // A variable to measure the size of the WAV header
+    const char* file_path;
     std::string input;
     
     // User is asked to input the file location of the desired WAV file.
     std::cout << "Input wave file name: ";
-    std::cin >> input;
-    filePath = input.c_str();
+    getline(std::cin, input);
+    file_path = input.c_str();
+    //Here, the filename is separated into characters and stored as a data stream in the memory. the variable 'file_path' points to the beginning of the data stream, which can be referenced when opening the file.
     
-    //
-    FILE* wavFile;
-    wavFile = fopen(filePath, "rb+");
+    // The inputted file is
+    FILE* wav_file;
+    wav_file = fopen(file_path, "rb+"); // 'rb+' is a binary read/update parameter. This allows the file to be read and separated into bytes easier, as well as updated if need be.
     
+    /*
     //Read the header
     size_t bytesRead = fread(&wav_hdr, 1, headerSize, wavFile);
     std::cout << bytesRead << std::endl;
     char buffer[] = { 'R' , 'I' , 'F' , 'X' };
     fseek(wavFile,0,SEEK_SET);
     fwrite(buffer, 2, 4, wavFile);
-     //Print the header
-    std::cout << "Chank ID: " << wav_hdr.RIFF[0] << wav_hdr.RIFF[1] << wav_hdr.RIFF[2] << wav_hdr.RIFF[3] << std::endl;
-    std::cout << "Chank Size: " << wav_hdr.ChunkSize << std::endl;
-    std::cout << "Format: " << wav_hdr.WAVE[0] << wav_hdr.WAVE[1] << wav_hdr.WAVE[2] << wav_hdr.WAVE[3] << std::endl;
-    std::cout << "SUb-Chank1 ID: " << wav_hdr.fmt[0] << wav_hdr.fmt[1] << wav_hdr.fmt[2] << wav_hdr.fmt[3] << std::endl;
-    std::cout << "Sub-Chank1 Size: " << wav_hdr.Subchunk1Size << std::endl;
-    std::cout << "Audio Format: " << wav_hdr.AudioFormat << std::endl;
-    std::cout << "Num of Channels: " << wav_hdr.NumOfChan << std::endl;
-    std::cout << "Sample Rate: " << wav_hdr.SamplesPerSec << std::endl;
-    std::cout << "Bits Per Sample: " << wav_hdr.BytesPerSec << std::endl;
-    std::cout << "Block Align: " << wav_hdr.blockAlign << std::endl;
-    std::cout << "Bits Per Sample: " << wav_hdr.bitsPerSample << std::endl;
-    std::cout << "Sub-Chank2 ID: " << wav_hdr.Subchunk2ID[0] << wav_hdr.Subchunk2ID[1] << wav_hdr.Subchunk2ID[2] << wav_hdr.Subchunk2ID[3] << std::endl;
-    std::cout << "Sub-Chank2 Size: " << wav_hdr.Subchunk2Size << std::endl;
-    fclose(wavFile);
+    */
+    
+    /* if (wav_file == NULL ){
+        std::cout << "cant read it mate";
+    } else {
+        std::cout << "poop";
+    }
+    */
+    
+    fclose(wav_file);
     return 0;
 }
