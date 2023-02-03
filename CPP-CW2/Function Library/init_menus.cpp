8ab53f9a6ed0f161a52c8e93acda3886c02158aa
alexsedman <alexjsedman@gmail.com>
2023-02-03 10:04:31 +0000

    test

---
+++ CPP-CW2/Function Library/init_menus.cpp
@@ -0,0 +1,8 @@
+//
+//  init_menus.cpp
+//  CPP-CW2
+//
+//  Created by Alex Sedman on 03/02/2023.
+//
+
+#include "init_menus.hpp"
---
+++ CPP-CW2/Function Library/init_menus.hpp
@@ -0,0 +1,13 @@
+//
+//  init_menus.hpp
+//  CPP-CW2
+//
+//  Created by Alex Sedman on 03/02/2023.
+//
+
+#ifndef init_menus_hpp
+#define init_menus_hpp
+
+#include <stdio.h>
+
+#endif /* init_menus_hpp */
